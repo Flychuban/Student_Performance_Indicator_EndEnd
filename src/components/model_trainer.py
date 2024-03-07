31f50c03756ca8bf23769d1fcb04bb7fc5812c36
Flychuban <kaloanas07@gmail.com>
2024-03-07 19:02:46 +0200

    add code for working like jupyter notebook

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -39,3 +39,7 @@ preprocessor = ColumnTransformer(
     ])
 
 X = preprocessor.fit_transform(X)
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+y_train.shape
